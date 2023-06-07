184bbe8fd8061dcc59925a48fd26fec72bdf805d
IamVine <donghyun.pv@gmail.com>
2023-06-07 17:45:59 +0900

    :sparkles:Feat:회원모델 생성 #3
    models.py에 회원 모델을 만들었습니다.
    -django custom user model 사용
    -회원으로부터 이메일, 닉네임, 프로필이미지, 성별, 나이, 비밀번호를
    받습니다.
    -프로필 이미지 업로드는 선택사항입니다. 아무것도 올리지 않으면 default
    이미지가 올라갑니다.
    -이메일과 닉네임은 타인과 겹치면 안되므로  unique=True 주었습니다.
    -성별은 필수값이지만 남성, 여성, 밝히고 싶지 않음으로 세가지 옵션을
    주었습니다.
    -회원생성 시 회원가입일, 마지막 회원정보 수정일이 자동으로 필드에 추가됩니다.
    -장고 커스텀유저 기능으로 last login 날짜시간이 필드에 추가됩니다.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, nickname, profile_image, gender, age, password=None):
+        """
+        요청 받은 이메일, 회원닉네임, 프로필이미지, 성별, 나이, 비밀번호로 유저를 생성하여 저장합니다.
+        """
+        if not email:
+            raise ValueError("유저 이메일은 필수입력 사항입니다.")
+
+        if not nickname:
+            raise ValueError("유저 닉네임은 필수입력 사항입니다.")
+
+        if not gender:
+            raise ValueError("유저 성별은 필수입력 사항입니다.")
+
+        if not age:
+            raise ValueError("유저 나이는 필수입력 사항입니다.")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname,
+            profile_image=profile_image,
+            gender=gender,
+            age=age,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password=None):
+        """
+        요청받은 이메일, 회원아이디, 비밀번호로 수퍼유저를 생성하여 저장합니다.
+        """
+        user = self.create_user(
+            email=email,
+            nickname=nickname,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="사용자 이메일",
+        max_length=255,
+        unique=True,
+    )
+
+    nickname = models.CharField(max_length=100, unique=True, verbose_name="사용자 닉네임")
+    password = models.CharField(max_length=255, verbose_name="비밀번호")
+
+    GENDER_CHOICES = [
+        ("남성", "남성"),
+        ("여성", "여성"),
+        ("밝히고 싶지 않음", "밝히고 싶지 않음"),
+    ]
+    gender = models.CharField(
+        max_length=10,
+        choices=GENDER_CHOICES,
+        blank=False,
+        error_messages="필수 입력 값입니다.",
+        verbose_name="성별",
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="아이디 생성일")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="마지막 회원정보 수정일")
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    username_FIELD = "email"  # 이걸로 로그인 하겠다 하는 필드. unique=True 속성이어야 함.
+    REQUIRED_FIELDS = ["nickname"]  # createsuperuser할때 어떤 필드들을 작성받을 지 적는 필드다.
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
