13efaae435790c06dda4b4e0a183f1d0e4544414
IamVine <donghyun.pv@gmail.com>
2023-06-07 21:19:02 +0900

    :sparkles:Feat:회원가입 views.py 생성 #3

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,3 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from users.models import User
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "회원가입이 완료되었습니다."}, status=status.HTTP_201_CREATED
+            )
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
