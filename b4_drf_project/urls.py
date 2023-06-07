5bb95ca83a1b6e991795a5971fd5bd7a3d2efaa5
Seman_Kim <sdoramkim@gmail.com>
2023-06-07 11:01:03 +0900

    :tada:Start: 백엔드 프로젝트 시작
    [필요]
    AUTH_USER_MODEL 설정
    AI 패키지 설치
    각 앱에서 urls 연결
    serializers.py 추가

---
+++ b4_drf_project/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("api/accompanies/", include("accompanies.urls")),
+    path("api/exhibitions/", include("exhibitions.urls")),
+    path("api/reviews/", include("reviews.urls")),
+    path("api/users/", include("users.urls")),
+    path("admin/", admin.site.urls),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
