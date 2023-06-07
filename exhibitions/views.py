63370386bad7812b2203e004726ff74baef45727
banghyunjae <banghyunjae@naver.com>
2023-06-07 15:04:23 +0900

    :sparkles:Feat: 전시회 목록을 불러옴 #12
    목록 불러오기!

--- exhibitions/views.py
+++ exhibitions/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Exhibition
+from .serializers import ExhibitionSerializer
 
-# Create your views here.
+
+class ExhibitionView(APIView):
+    def get(self, request):  # 전시회 목록 불러오기
+        exhibitions = Exhibition.objects.all()
+        serializer = ExhibitionSerializer(exhibitions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
