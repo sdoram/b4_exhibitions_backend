179c75db9c01574e3beddc20bdde868c4abe825f
banghyunjae <banghyunjae@naver.com>
2023-06-07 16:28:11 +0900

    :hammer:Refact: urls 수정 완료
    잘못 작성된 urls 수정완료

--- exhibitions/urls.py
+++ exhibitions/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ExhibitionView, ExhibitionDetailView, ExhibitionLikeView
 
 urlpatterns = [
-    path("exhibitions/", ExhibitionView.as_view(), name="exhibition"),
-    path("exhibitions/<int:pk>/", ExhibitionDetailView.as_view(), name="exhibition-detail"),
-    path("exhibitions/<int:pk>/like/", ExhibitionLikeView.as_view(), name="exhibition-like"),
+    path("", ExhibitionView.as_view(), name="exhibition"),
+    path("<int:exhibitions_id>/", ExhibitionDetailView.as_view(), name="exhibition-detail"),
+    path("<int:exhibitions_id>/like/", ExhibitionLikeView.as_view(), name="exhibition-like"),
 ]
