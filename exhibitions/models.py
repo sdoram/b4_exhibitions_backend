5a2c5c0c326194afff3037f9a74e60685bef7dec
banghyunjae <banghyunjae@naver.com>
2023-06-07 15:58:41 +0900

    ❌Remove: period 삭제
    불필요한 필드 삭제

--- exhibitions/models.py
+++ exhibitions/models.py
@@ -6,7 +6,6 @@ class Exhibition(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     info_name = models.CharField("전시회 이름", max_length=100)
     content = models.TextField("전시회 내용")
-    period = models.CharField("전시회 기간", max_length=100)
     location = models.CharField("전시회 장소", max_length=100)
     image = models.ImageField("이미지", blank=True, null=True)
     created_at = models.DateTimeField("생성시간", auto_now_add=True)
