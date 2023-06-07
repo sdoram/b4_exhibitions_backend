b65116734e1aa2ca4942e31173db39453e202297
minjeong <ehdro418@naver.com>
2023-06-07 17:43:42 +0900

    :sparkles:Feat: Create Accompany Model
    동행구하기 모델 생성

--- accompanies/models.py
+++ accompanies/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from exhibitions.models import Exhibition
 
-# Create your models here.
+
+class Accompany(models.Model):
+    user = models.ManyToManyField(
+        User,
+        related_name="get_accompanies",
+        verbose_name="동행구하기 작성자",
+    )
+    info_name = models.ManyToManyField(
+        Exhibition,
+        related_name="accompanies",
+        verbose_name="전시회 정보",
+    )
+    content = models.TextField("동행구하기 내용")
+    personnel = models.PositiveIntegerField("동행인원")
+    start_time = models.DateTimeField("모임시작시간", blank=True, null=True)
+    end_time = models.DateTimeField("모임종료시간", blank=True, null=True)
+    created_at = models.DateTimeField("생성시간", auto_now_add=True)
+    updated_at = models.DateTimeField("수정시간", auto_now=True)
+
+    def __str__(self):
+        return str(self.content)
