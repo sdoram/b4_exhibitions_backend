9230e60616274ca0d95735b8b015edd592065880
banghyunjae <banghyunjae@naver.com>
2023-06-07 12:14:00 +0900

    :sparkles:Feat: Exhibitions 모델, 뷰, urls, 시리얼라이져, 어드민 기능 구현

--- exhibitions/admin.py
+++ exhibitions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Exhibition
 
-# Register your models here.
+
+@admin.register(Exhibition)
+class ExhibitionAdmin(admin.ModelAdmin):
+    list_display = ("info_name", "period", "location", "created_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("info_name", "content", "location")
--- exhibitions/views.py
+++ exhibitions/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Exhibition
+from .serializers import ExhibitionSerializer
 
-# Create your views here.
+
+class ExhibitionView(APIView):
+    def get(self, request):  # 전시회 목록 불러오기
+        exhibitions = Exhibition.objects.all()
+        serializer = ExhibitionSerializer(exhibitions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):  # 전시회 작성
+        serializer = ExhibitionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "게시글이 등록되었습니다."}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "요청이 올바르지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ExhibitionDetailView(APIView):
+    def get_object(self, exhibition_id):  # 객체가 없을때 404
+        try:
+            return Exhibition.objects.get(id=exhibition_id)
+        except Exhibition.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, exhibition_id):
+        exhibition = self.get_object(exhibition_id)
+        serializer = ExhibitionSerializer(exhibition)
+        return Response(serializer.data)
+
+    def put(self, request, exhibition_id):
+        exhibition = self.get_object(exhibition_id)
+        serializer = ExhibitionSerializer(exhibition, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "게시글이 수정되었습니다."}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, exhibition_id):
+        exhibition = self.get_object(exhibition_id)
+        exhibition.delete()
+        return Response({"message": "게시글이 삭제되었습니다."}, status=status.HTTP_204_NO_CONTENT)
+
+
+class ExhibitionLikeView(APIView):  # 좋아요 기능
+    def post(self, request, exhibition_id):
+        exhibition = get_object_or_404(Exhibition, id=exhibition_id)
+        if request.user not in exhibition.likes.all():
+            exhibition.likes.add(request.user)
+            return Response({"message": "좋아요"}, status=status.HTTP_201_CREATED)
+        else:
+            exhibition.likes.remove(request.user)
+            return Response({"message": "좋아요 취소"}, status=status.HTTP_204_NO_CONTENT)
