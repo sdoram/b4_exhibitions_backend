ae954bcd512e4e4b2227afa42729ab7948f23312
banghyunjae <banghyunjae@naver.com>
2023-06-07 15:19:56 +0900

    :sparkles:Feat: urls패턴 구현
    변경사항이 있다면 바꿀 예정입니다.

--- exhibitions/urls.py
+++ exhibitions/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from .views import ExhibitionView, ExhibitionDetailView, ExhibitionLikeView
+
+urlpatterns = [
+    path("exhibitions/", ExhibitionView.as_view(), name="exhibition"),
+    path("exhibitions/<int:pk>/", ExhibitionDetailView.as_view(), name="exhibition-detail"),
+    path("exhibitions/<int:pk>/like/", ExhibitionLikeView.as_view(), name="exhibition-like"),
+]
