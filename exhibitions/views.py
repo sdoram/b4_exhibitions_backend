a03dd77960d94abea0a6e92aaed78d584249eb2e
banghyunjae <banghyunjae@naver.com>
2023-06-07 15:32:21 +0900

    :sparkles:Feat: 게시물 작성 추가 #13
    exhibitionView에서 전시회 전체 목록과 전시회를 등록 할 수 있습니다.

--- exhibitions/views.py
+++ exhibitions/views.py
@@ -11,3 +11,11 @@ class ExhibitionView(APIView):
         exhibitions = Exhibition.objects.all()
         serializer = ExhibitionSerializer(exhibitions, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):  # 전시회 작성
+        serializer = ExhibitionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "게시글이 등록되었습니다."}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "요청이 올바르지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
