f330494f410db5e62375fec9cc109535c2758266
IamVine <donghyun.pv@gmail.com>
2023-06-08 11:27:57 +0900

    :package:Package:환경변수 관리 방법 .env로 변경
    환경변수 관리 방법을 기존 my_settings.py 에서
    dotenv를 사용한 방법으로 바꾸었습니다.
    이제 숨기고 싶은 환경변수는 .env 파일에 저장해주세요.
    !! 덧붙여 requirements.txt에 dot env가 추가되었습니다.
    새롭게 pip install 해주세요.

--- b4_drf_project/settings.py
+++ b4_drf_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import my_settings
 import os
 from datetime import timedelta
 
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = my_settings.SECRET
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
