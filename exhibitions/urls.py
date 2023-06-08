53c200c362979027251bd8542f073e70b4437395
banghyunjae <banghyunjae@naver.com>
2023-06-08 14:51:45 +0900

    :ambulance:Hotfix: exhibition_id 인자 값을 찾지 못해서 수정
    포스트맨 활용중 게시글 아이디의 값을 찾지 못하여서
    exhibitions_id 를 >> exhibition_id로 수정

--- exhibitions/urls.py
+++ exhibitions/urls.py
@@ -3,6 +3,6 @@ from .views import ExhibitionView, ExhibitionDetailView, ExhibitionLikeView
 
 urlpatterns = [
     path("", ExhibitionView.as_view(), name="exhibition"),
-    path("<int:exhibitions_id>/", ExhibitionDetailView.as_view(), name="exhibition-detail"),
-    path("<int:exhibitions_id>/like/", ExhibitionLikeView.as_view(), name="exhibition-like"),
+    path("<int:exhibition_id>/", ExhibitionDetailView.as_view(), name="exhibition-detail"),
+    path("<int:exhibition_id>/like/", ExhibitionLikeView.as_view(), name="exhibition-like"),
 ]
