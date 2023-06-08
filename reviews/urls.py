c625db7c67f3f13858de6cffd61ae2e880f10e15
mijinleee <alwls1785@gmail.com>
2023-06-08 17:33:07 +0900

    :sparkles:Feat:urls 추가
    url패턴구현

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+from .views import ReviewView, ReviewDetailView
+
+urlpatterns = [
+    path("<int:exhibitions_id>/", ReviewView.as_view(), name="review"),
+    path(
+        "detail/<int:exhibitions_id>/", ReviewDetailView.as_view(), name="review-detail"
+    ),
+]
