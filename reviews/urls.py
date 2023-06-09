9a13d07cacbf5cb22e3d16c9b413a11465173de2
mijinleee <alwls1785@gmail.com>
2023-06-09 12:24:49 +0900

    :bug:Fix:review url 수정
    exhibitions를 exhibition으로  수정

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ReviewView, ReviewDetailView
 
 urlpatterns = [
-    path("<int:exhibitions_id>/", ReviewView.as_view(), name="review"),
+    path("<int:exhibition_id>/", ReviewView.as_view(), name="review"),
     path("detail/<int:review_id>/", ReviewDetailView.as_view(), name="review-detail"),
 ]
