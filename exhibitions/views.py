5e602cc0374da5e73c4f909c2d587f844c5c45a5
banghyunjae <banghyunjae@naver.com>
2023-06-09 13:46:32 +0900

    :hammer:Refact: 글 전체조회 권한설정 #12
    인증된 사용자, 인증되지 않은사용자 모두 읽기만 가능합니다.

--- exhibitions/views.py
+++ exhibitions/views.py
@@ -1,7 +1,7 @@
 from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
 
@@ -10,6 +10,8 @@ from .serializers import ExhibitionSerializer
 
 
 class ExhibitionView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]  # 인증된 사용자, 인증되지 않은 사용자 모두 읽기 가능
+
     def get(self, request):  # 전시회 목록 불러오기
         # 카테고리 정보 가져오기
         category = request.query_params.get("category", None)
@@ -29,26 +31,28 @@ class ExhibitionView(APIView):
         serializer = ExhibitionSerializer(paginated_exhibitions, many=True)
         return pagination.get_paginated_response(serializer.data)
 
-    permission_classes = [IsAdminUser]
-
     def post(self, request):  # 전시회 작성
-        serializer = ExhibitionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response({"message": "게시글이 등록되었습니다."}, status=status.HTTP_201_CREATED)
+        if request.user.is_staff:
+            serializer = ExhibitionSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(user=request.user)
+                return Response({"message": "게시글이 등록되었습니다."}, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message": "요청이 올바르지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({"message": "요청이 올바르지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "관리자만 글을 작성할 수 있습니다."}, status=status.HTTP_403_FORBIDDEN)
 
 
 class ExhibitionDetailView(APIView):
-    permission_classes = [IsAuthenticated]
+    def get_permissions(self):
+        if self.request.method in ["PUT", "DELETE"]:
+            return [IsAdminUser()]
+        return [IsAuthenticatedOrReadOnly()]
 
     def get(self, request, exhibition_id):
         exhibition = get_object_or_404(Exhibition, id=exhibition_id)
         serializer = ExhibitionSerializer(exhibition)
         return Response(serializer.data)
-
-    permission_classes = [IsAdminUser]
 
     def put(self, request, exhibition_id):
         exhibition = get_object_or_404(Exhibition, id=exhibition_id)
