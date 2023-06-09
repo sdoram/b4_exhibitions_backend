5eb8356ae585cd9de699b17741eeaac5d8f6e5c2
mijinleee <alwls1785@gmail.com>
2023-06-09 10:22:20 +0900

    :hammer:Refact:url 수정
    url 수정했습니다.

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,7 +3,5 @@ from .views import ReviewView, ReviewDetailView
 
 urlpatterns = [
     path("<int:exhibitions_id>/", ReviewView.as_view(), name="review"),
-    path(
-        "detail/<int:exhibitions_id>/", ReviewDetailView.as_view(), name="review-detail"
-    ),
+    path("detail/<int:review_id>/", ReviewDetailView.as_view(), name="review-detail"),
 ]
