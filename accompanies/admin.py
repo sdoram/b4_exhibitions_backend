3f8105acc15b922b44a6437218bca9b5f5dfb677
minjeong <ehdro418@naver.com>
2023-06-09 14:33:44 +0900

    :sparkles:Feat: Change Admin Page of Accompany
    어드민 페이지에서 동행 구하기 댓글 편집할 때 동행 신청하기 댓글도 같이
    편집할 수 있게 수정했습니다.
    동행 구하기 댓글 목록에는 전시회, 내용, 작성자, 동행인원, 모임시작시간,
    종료시간이 나오게 했습니다.

--- accompanies/admin.py
+++ accompanies/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Accompany, Apply
 
-admin.site.register(Accompany)
-admin.site.register(Apply)
+
+class ApplyInline(admin.TabularInline):
+    model = Apply
+
+
+@admin.register(Accompany)
+class AccompanyAdmin(admin.ModelAdmin):
+    list_display = [
+        "exhibition",
+        "content",
+        "user",
+        "personnel",
+        "start_time",
+        "end_time",
+    ]
+    list_display_links = ["content"]
+
+    inlines = [ApplyInline]
