16c3368f74145229deed332d65ef0b834866f222
mijinleee <alwls1785@gmail.com>
2023-06-09 09:47:30 +0900

    :sparkles:Feat:reivew 보기 기능 추가 #18
    리뷰 보기 기능 추가했습니다.

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Review
+from .serializers import ReviewSerializer, ReviewCreateSerializer
+
+
+class ReviewView(APIView):
+    # 리뷰 보기
+    def get(self, request, exhibition_id):
+        reviews = Review.objects.filter(exhibition_id=exhibition_id)
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # 리뷰 작성
+    def post(self, request, exhibition_id):
+        # 로그인
+        if not request.user.is_authenticated:
+            return Response(
+                {"message": "로그인이 필요합니다."}, status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        serializer = ReviewCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(exhibition_id=exhibition_id, user=request.user)
+            return Response({"message": "리뷰가 등록되었습니다."}, status=status.HTTP_201_CREATED)
+        return Response(
+            {"message": "요청이 올바르지 않습니다."}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class ReviewDetailView(APIView):
+    # 리뷰 수정
+    def put(self, request, exhibition_id, id_review):
+        review = get_object_or_404(Review, exhibition_id=exhibition_id, id=id_review)
+        if request.user == review.user:
+            serializer = ReviewCreateSerializer(review, data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({"message": "리뷰가 수정되었습니다."}, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"message": "권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
+
+    # 삭제
+    def delete(self, request, exhibition_id, id_review):
+        review = get_object_or_404(Review, exhibition_id=exhibition_id, id=id_review)
+
+        if request.user == review.user:
+            review.delete()
+            return Response({"message": "삭제되었습니다."}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({"message": "권한이 없습니다."}, status=status.HTTP_403_FORBIDDEN)
