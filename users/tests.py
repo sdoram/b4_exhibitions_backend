c56545ce8a3e49dce62c8b295df8ce37fec783f8
minjeong <ehdro418@naver.com>
2023-06-20 12:39:45 +0900

    :umbrella:Test: 회원가입 테스트코드 추가 #53

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+# ---------------------------회원가입 테스트------------------------
+class SignupViewTest(APITestCase):
+    def test_signup(self):
+        url = reverse("users:user-signup")
+        data = {
+            "email": "test@test.com",
+            "nickname": "test",
+            "password": "123",
+            "password_check": "123",
+            "gender": "여성",
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
