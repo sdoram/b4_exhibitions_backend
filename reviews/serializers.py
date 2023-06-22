76491d83dd15b33a1a101841cf9ca7cfa36c6962
minjeong <ehdro418@naver.com>
2023-06-22 23:55:58 +0900

    :sparkles:Feat: 리뷰 시리얼라이저 user_id 필드 user로 변경 #18

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -4,13 +4,12 @@ from .models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
     nickname = serializers.SerializerMethodField()
-    user_id = serializers.SerializerMethodField()
 
     class Meta:
         model = Review
         fields = [
             "id",
-            "user_id",
+            "user",
             "nickname",
             "exhibition_id",
             "content",
@@ -22,9 +21,6 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     def get_nickname(self, obj):
         return obj.user.nickname
-
-    def get_user_id(self, obj):
-        return obj.user.id
 
 
 class ReviewCreateSerializer(serializers.ModelSerializer):
