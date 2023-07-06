531a3bbf148614d10b4afbfdd876a0acf02273cf
minjeong <ehdro418@naver.com>
2023-07-06 19:32:46 +0900

    :sparkles:Feat: 리뷰 어드민 페이지 구성

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Review
 
-admin.site.register(Review)
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = [
+        "exhibition",
+        "content",
+        "user",
+    ]
+    list_display_links = ["content"]
