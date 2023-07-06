7baa61c26e9662903357e3710cef64c8ee52c7bc
minjeong <ehdro418@naver.com>
2023-07-06 19:29:47 +0900

    :hammer:Refact: 동행 시리얼라이저 통합 및 불필요한 코드 삭제 #142

--- accompanies/serializers.py
+++ accompanies/serializers.py
@@ -4,62 +4,21 @@ from accompanies.models import Accompany, Apply
 
 class ApplySerializer(serializers.ModelSerializer):
     nickname = serializers.SerializerMethodField()
-    user = serializers.SerializerMethodField()
-    accompany = serializers.SerializerMethodField()
 
     class Meta:
         model = Apply
         fields = "__all__"
-
-    def get_nickname(self, obj):
-        return obj.user.nickname
-
-    def get_user(self, obj):
-        return obj.user.id
-
-    def get_accompany(self, obj):
-        return obj.accompany.id
-
-
-class AccompanyCreateSerializer(serializers.ModelSerializer):
-    nickname = serializers.SerializerMethodField()
-    user = serializers.SerializerMethodField()
-    picks_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Accompany
-        fields = (
+        read_only_fields = (
             "id",
             "user",
             "nickname",
-            "content",
-            "personnel",
-            "start_time",
-            "end_time",
+            "accompany",
+            "created_at",
             "updated_at",
-            "picks_count",
         )
-
-    read_only_fields = (
-        "nickname",
-        "updated_at",
-    )
-
-    def validate(self, data):
-        if data["start_time"] > data["end_time"]:
-            raise serializers.ValidationError("종료시간은 시작시간보다 빠를 수 없습니다.")
-        if data["personnel"] == 0:
-            raise serializers.ValidationError("목표인원을 1명 이상 선택하십시오.")
-        return data
 
     def get_nickname(self, obj):
         return obj.user.nickname
-
-    def get_user(self, obj):
-        return obj.user.id
-
-    def get_picks_count(self, obj):
-        return obj.picks.count()
 
 
 class AccompanySerializer(serializers.ModelSerializer):
@@ -70,13 +29,33 @@ class AccompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Accompany
         fields = "__all__"
+        read_only_fields = (
+            "id",
+            "user",
+            "nickname",
+            "exhibition",
+            "applies",
+            "created_at",
+            "updated_at",
+        )
 
-    def get_nickname(self, obj):
-        return obj.user.nickname
+    def validate(self, data):
+        start_time = data.get("start_time")
+        end_time = data.get("end_time")
+        personnel = data.get("personnel")
+
+        if start_time and end_time and start_time > end_time:
+            raise serializers.ValidationError("종료시간은 시작시간보다 빠를 수 없습니다.")
+        if personnel and personnel == 0:
+            raise serializers.ValidationError("목표인원을 1명 이상 선택하십시오.")
+        return data
 
     def get_applies(self, obj):
         applies = obj.applies.all().order_by("-updated_at")
         return ApplySerializer(applies, many=True).data
+
+    def get_nickname(self, obj):
+        return obj.user.nickname
 
     def get_picks_count(self, obj):
         return obj.picks.count()
